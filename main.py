f3a3b61c36c4f01bf5c22fc0c3824807a27189f6
Ibhax <botibha@gmail.com>
2023-11-05 11:55:23 +0000

    add status

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Ibhax API"}
+    return {"message": "Ibhax API", "status": "Active"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
